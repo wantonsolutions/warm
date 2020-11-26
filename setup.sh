32b2bb6c8fae187cec8547e2d3a68a14d87686ef
wantonsolutions <stewbertgrant@gmail.com>
2020-11-26 07:59:44 -0800

    added setup for yak02

--- setup.sh
+++ setup.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
-hname=`hostname`
-case $hname in
-    "yak-00.sysnet.ucsd.edu")
-        iface="enp4s0"
-        ipaddr="192.168.1.12"
-    ;;
-    "yak-01.sysnet.ucsd.edu")
-        iface="enp129s0"
-        ipaddr="192.168.1.13"
-    ;;
+function setup_nic {
+    hname=`hostname`
+    echo "Configuring NIC on $hname"
+    case $hname in
+        "yak-00.sysnet.ucsd.edu")
+            iface="enp4s0"
+            ipaddr="192.168.1.12"
+        ;;
+        "yak-01.sysnet.ucsd.edu")
+            iface="enp129s0"
+            ipaddr="192.168.1.13"
+        ;;
+        "yak-02.sysnet.ucsd.edu")
+            iface="enp4s0"
+            ipaddr="192.168.1.14"
+        ;;
 
-    *)
-    echo "$machine does not have a configuration set in this script. Exiting and doing nothing"
-    exit 1
-    ;;
-esac
+        *)
+        echo "$machine does not have a configuration set in this script. Exiting and doing nothing"
+        exit 1
+        ;;
+    esac
 
-echo "Configuring Dumb NIC on $hname"
-#set the link up
-sudo ip link set $iface up
-#turn the interface on and configure ip
-sudo ifconfig enp4s0 $ipaddr netmask 255.0.0.0 
-echo "Configuration complete"
+    #set the link up
+    sudo ip link set $iface up
+    #turn the interface on and configure ip
+    sudo ifconfig $iface $ipaddr netmask 255.0.0.0 
+    echo "Nic Configuration complete Configuration complete"
+}
+
+function setup_hugepages {
+    echo "Setting up hugepages"
+    sudo hugeadm --pool-pages-min 2MB:8192
+    sudo mkdir -p /mnt/hugetlbfs ; mount -t hugetlbfs none /mnt/hugetlbfs
+    sudo numactl --physcpubind=0 --localalloc LD_PRELOAD=libhugetlbfs.so HUGETLB_MORECORE=yes
+    echo "HUGEPAGE INFO"
+    cat /proc/meminfo | grep Huge
+    echo "Hugepage setup complete"
+}
+
+setup_nic
+setup_hugepages
 exit 0
