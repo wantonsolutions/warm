be89d24033f6e7648724f0eb4bdfd08b093ead22
wantonsolutions <stewbertgrant@gmail.com>
2021-02-07 11:37:59 -0800

    added packet size to driver

--- control/throughput_driver.sh
+++ control/throughput_driver.sh
@@ -2,20 +2,26 @@
 #rm results.dat
 date=`date`
 echo ">>>>>>>>> ($date)" >> results.dat
-threads=("1" "2" "4" "8")
-opmodes=("MITSUME_YCSB_MODE_A" "MITSUME_YCSB_MODE_B" "MITSUME_YCSB_MODE_C")
+#threads=("1" "2" "4" "8")
+threads=("16")
+#opmodes=("MITSUME_YCSB_MODE_A" "MITSUME_YCSB_MODE_B" "MITSUME_YCSB_MODE_C")
+#packet_size=("1000" "500" "250" "125" "64" "32")
+packet_size=("500")
 #opmodes=("MITSUME_YCSB_MODE_C")
 #keyspaces=("100" "1000" "10000" "100000")
-keyspaces=("100000")
+keyspaces=("1000")
 
 for k in ${keyspaces[@]}; do
     for i in ${opmodes[@]}; do
         for j in ${threads[@]}; do
-            echo "op modes: $i"
-            echo "Threads: $j"
-            echo "Keys: $k"
-            ./throughput_bench.sh $j $k $i
-            #exit 1
+            for l in ${packet_size[@]}; do
+                echo "op modes: $i"
+                echo "Threads: $j"
+                echo "Keys: $k"
+                echo "Packet Size: $l"
+                ./throughput_bench.sh $j $k $i $l
+                #exit 1
+            done
         done
     done
 done
