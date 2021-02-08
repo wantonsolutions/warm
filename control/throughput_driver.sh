16fcacaca0686db7f5c2dbbc29e81e9413128e43
wantonsolutions <ssgrant@eng.ucsd.edu>
2021-02-08 09:01:06 -0800

    throughput driver threads

--- control/throughput_driver.sh
+++ control/throughput_driver.sh
@@ -3,10 +3,11 @@
 date=`date`
 echo ">>>>>>>>> ($date)" >> results.dat
 #threads=("1" "2" "4" "8")
-threads=("16")
+threads=("4")
 #opmodes=("MITSUME_YCSB_MODE_A" "MITSUME_YCSB_MODE_B" "MITSUME_YCSB_MODE_C")
 #packet_size=("1000" "500" "250" "125" "64" "32")
-packet_size=("500")
+packet_size=("500" "250" "125" "64" "32")
+#packet_size=("1000" "900" "800" "700" "600" " 500")
 #opmodes=("MITSUME_YCSB_MODE_A" "MITSUME_YCSB_MODE_B" "MITSUME_YCSB_MODE_C")
 opmodes=("MITSUME_YCSB_MODE_A")
 #keyspaces=("100" "1000" "10000" "100000")
