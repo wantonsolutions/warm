40d3a9007cf09fe326f4b9f90d55089021544bc1
wantonsolutions <ssgrant@eng.ucsd.edu>
2021-02-08 11:06:29 -0800

    added words and first contention mittigation result

---
+++ presentation/first_result/first_result.py
@@ -0,0 +1,53 @@
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+labels = ['1', '2', '4', '8', '16', '32']
+
+def div_thousand (list):
+    return [val /1000.0 for val in list]
+
+
+write_control_means = [74722.000000,133416.500000,224747.625000,355556.937500,510859.406250,316737.500000 ]
+default_means = [ 75163.101562 ,132205.703125,213194.703125,310103.218750,375318.468750,268242.000000 ]
+
+write_control_means=div_thousand(write_control_means)
+default_means = div_thousand(default_means)
+
+
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, write_control_means, width, label='Clover with RDMA write ordering')
+rects2 = ax.bar(x + width/2, default_means, width, label='Clover Default')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('KOP/s')
+ax.set_title('Clover YCSB-A (50% write)')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.set_xlabel("Threads");
+ax.legend()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+#autolabel(rects1)
+#autolabel(rects2)
+
+fig.tight_layout()
+plt.savefig("first.pdf")
+plt.show()
