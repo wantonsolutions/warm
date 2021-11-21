d97690af17b678b25b598f2fa09c76088be03a2d
wantonsolutions <stewbertgrant@gmail.com>
2021-11-21 11:15:59 -0800

    big safty before doing multiple rx cores

--- rmemc-dpdk/run.sh
+++ rmemc-dpdk/run.sh
@@ -27,12 +27,12 @@ pushd build
 
 #sudo -E ./basicfwd -l 0,2,4,6,8,10,12,14,16,18,20,22,24 -n 18
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6 -n 4
-echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6,8,10,12,14 -n 8
+#echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6,8,10,12,14 -n 8
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2 -n 4
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0 -n 1
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6 -n 4
-#echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2 -n 2
-#echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6,8,10,12,14,16,18,20,22,24 -n 18
+echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2 -n 2
+#echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6,8,10,12,14,16,18,20,22 -n 12
 #gdb --args rmemc-dpdk -l 0 -n 1
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0 -n 1
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2 -n 2
