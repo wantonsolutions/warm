ef5b14c438cf2684e3d50931044e8d7d6082b24e
wantonsolutions <stewbertgrant@gmail.com>
2021-11-21 12:08:08 -0800

    it might have literally been that simple

--- rmemc-dpdk/run.sh
+++ rmemc-dpdk/run.sh
@@ -31,7 +31,7 @@ pushd build
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2 -n 4
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0 -n 1
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6 -n 4
-echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4 -n 3
+echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6 -n 4
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0,2,4,6,8,10,12,14,16,18,20,22 -n 12
 #gdb --args rmemc-dpdk -l 0 -n 1
 #echo "iwicbV15" | sudo -S -E ./rmemc-dpdk -l 0 -n 1
